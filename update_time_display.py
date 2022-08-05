fa8cef45b04cff8dea188002ee4e28491cce9d42
Shinshi <jiankey.zjgsu@gmail.com>
2022-08-05 10:02:29 +0800

    Update update_time_display.py

--- update_time_display.py
+++ update_time_display.py
@@ -29,7 +29,7 @@ def main():
     # 替换 ---start--- 到 ---end--- 之间的内容
     # pytz.timezone('Asia/Shanghai')).strftime('%Y年%m月%d日%H时M分')
 #     fmt = '%Y-%m-%d %H:%M:%S %Z%z'
-    insert_info = "## Recent Blog Posts (Update Time:"+  datetime.fromtimestamp(int(time.time()),pytz.timezone('Asia/Shanghai')).strftime('%Y/%m/%d') + " | Update by Github Actions)"
+    insert_info = "## Recent Blog Posts (UTC+8 Update Time:"+  datetime.fromtimestamp(int(time.time()),pytz.timezone('Asia/Shanghai')).strftime('%Y/%m/%d') + " | Update by Github Actions)"
     # 获取README.md内容
     with open (os.path.join(os.getcwd(), "README.md"), 'r', encoding='utf-8') as f:
         readme_md_content = f.read()
