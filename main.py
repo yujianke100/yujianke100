ca486c7dd50a72e83a85aa278dfb0b32bea2841c
Yu Jianke <jiankey.zjgsu@gmail.com>
2022-09-28 17:16:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_link_info(feed_url, num):
     
 def main():
     utc_now = datetime.utcnow().replace(tzinfo=timezone.utc)
-    insert_info = "<!--BLOG_START-->\n## Recent Blog Posts\n *Update Time: "+ utc_now.astimezone(SHA_TZ).strftime('%Y-%m-%d %H:%M') + " (UTC+8) | Updated by Github Actions*\n" + get_link_info("https://www.cnblogs.com/yujianke/rss", 5) + "<!--BLOG_END-->"
+    insert_info = "<!--BLOG_START-->\n## Recent Blog Posts\n *Update Time: "+ utc_now.astimezone(SHA_TZ).strftime('%Y-%m-%d %H:%M') + " (UTC+8) | Updated by Github Actions*\n" + get_link_info("https://feed.cnblogs.com/blog/u/769089/rss/", 5) + "<!--BLOG_END-->"
     print(insert_info)
     with open (os.path.join(os.getcwd(), "README.md"), 'r', encoding='utf-8') as f:
         readme_md_content = f.read()
